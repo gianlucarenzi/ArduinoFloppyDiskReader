9552e0f4d90c35473c7eb52f7daf6a5383834809
Gianluca Renzi <gianlucarenzi@eurek.it>
2019-06-24 11:54:01 +0200

    Avoid gcc warning

--- ArduinoFloppyReader/AVRFloppyReaderQt/avrfloppyQt/clicklabel.cpp
+++ ArduinoFloppyReader/AVRFloppyReaderQt/avrfloppyQt/clicklabel.cpp
@@ -2,11 +2,14 @@
 
 ClickLabel::ClickLabel(QWidget* parent, Qt::WindowFlags f)
     : QLabel(parent) {
-
+    // Avoid gcc warning
+    f = f;
 }
 
 ClickLabel::~ClickLabel() {}
 
 void ClickLabel::mousePressEvent(QMouseEvent* event) {
+    // Avoid gcc warning
+    event = event;
     emit clicked();
 }
