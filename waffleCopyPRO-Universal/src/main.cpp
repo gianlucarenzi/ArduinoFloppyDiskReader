f2b05ed4628083ecceb0668f5cad9b2f7358c104
gianlucarenzi <icjtqr@gmail.com>
2022-01-07 17:55:04 +0100

    Added Amiga Topaz Font

--- waffleCopyPRO-Universal/src/main.cpp
+++ waffleCopyPRO-Universal/src/main.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFont>
+#include <QFontDatabase>
+#include <QString>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    int id = QFontDatabase::addApplicationFont("fonts/TopazPlus_a500_v1.0.ttf");
+    QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+    QFont amigaTopaz(family);
     MainWindow w;
+    w.setFont(amigaTopaz);
     w.show();
     return a.exec();
 }
