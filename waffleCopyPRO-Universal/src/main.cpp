1327367ef552e7842083f4bfc5114cd876ea050e
Gianluca Renzi <gianlucarenzi@eurek.it>
2023-01-12 11:01:26 +0100

    Added VNC support

--- waffleCopyPRO-Universal/src/main.cpp
+++ waffleCopyPRO-Universal/src/main.cpp
@@ -7,6 +7,26 @@
 
 int main(int argc, char *argv[])
 {
+    bool useVNC = false;
+    for (int i=0; i < argc; i++)
+    {
+        QString arg = argv[i];
+        if (arg.contains("vnc"))
+        {
+            useVNC = true;
+        }
+    }
+
+    // Start an external Xorg based viewer
+    if (useVNC)
+    {
+        int i = system("./runvnc.sh");
+        if (i < 0)
+        {
+            fprintf(stderr, "*** System Call to runvnc.sh returns %d ERROR ***\n", i);
+        }
+    }
+
     QApplication a(argc, argv);
     int id = QFontDatabase::addApplicationFont("fonts/TopazPlus_a500_v1.0.ttf");
     QString family = QFontDatabase::applicationFontFamilies(id).at(0);
