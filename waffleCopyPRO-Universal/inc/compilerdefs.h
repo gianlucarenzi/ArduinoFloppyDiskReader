4b8f29089094d998b65975ff24febe88d5530049
Gianluca Renzi <icjtqr@gmail.com>
2025-12-26 21:55:39 +0100

    update compiler defs

--- waffleCopyPRO-Universal/inc/compilerdefs.h
+++ waffleCopyPRO-Universal/inc/compilerdefs.h
@@ -1,6 +1,11 @@
 #ifndef COMPILERDEFS_H
 #define COMPILERDEFS_H
 
+#ifdef _WIN32
+#include <winsock2.h>
+#include <windows.h>
+#endif
+
 #if !defined(__PRETTY_FUNCTION__) && !defined(__GNUC__)
 #define __PRETTY_FUNCTION__ __FUNCSIG__
 #endif
