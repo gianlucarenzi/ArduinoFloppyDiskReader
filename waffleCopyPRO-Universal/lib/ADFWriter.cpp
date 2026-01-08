a6c2f94bea0fba196ac4f33cd0c9d18f8474e49b
Gianluca Renzi <gianlucarenzi@eurek.it>
2026-01-08 15:26:51 +0100

    Missing includes for socket for Windows MSVC

--- waffleCopyPRO-Universal/lib/ADFWriter.cpp
+++ waffleCopyPRO-Universal/lib/ADFWriter.cpp
@@ -53,6 +53,8 @@ const long long StreamMax = std::numeric_limits<std::streamsize>::max();
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <Windows.h>
 #pragma comment(lib,"Ws2_32.lib")
 #else
