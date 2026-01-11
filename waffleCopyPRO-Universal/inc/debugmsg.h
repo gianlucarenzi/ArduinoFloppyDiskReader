fd2f400df2cb505f92975e5ab821cce4a1def55e
Gianluca Renzi <icjtqr@gmail.com>
2026-01-12 00:14:24 +0100

    Update waffleCopyPRO-Universal/inc/debugmsg.h

--- waffleCopyPRO-Universal/inc/debugmsg.h
+++ waffleCopyPRO-Universal/inc/debugmsg.h
@@ -1,16 +1,29 @@
 #ifndef DEBUGMSG_H
 #define DEBUGMSG_H
 
-#include <QString>
+class QString;
+
+#if defined(__GNUC__) || defined(__clang__)
+#define NO_INSTRUMENT __attribute__((no_instrument_function))
+#else
+#define NO_INSTRUMENT
+#endif
 
 class DebugMsg
 {
 public:
-    static void init(bool enabled);
-    static void print(const char* func, const QString& msg);
+    static void init(bool enabled) NO_INSTRUMENT;
+    static void print(const char* func, const QString& msg) NO_INSTRUMENT;
+    static bool enabled() NO_INSTRUMENT;
 
 private:
     static bool s_enabled;
 };
+
+#ifndef DEBUGMSG
+#include <stdio.h>
+#define DEBUGMSG(fmt, ...) \
+    do { if (DebugMsg::enabled()) { printf("%s: " fmt "\n", __func__, ##__VA_ARGS__); } } while(0)
+#endif // DEBUGMSG
 
 #endif // DEBUGMSG_H
