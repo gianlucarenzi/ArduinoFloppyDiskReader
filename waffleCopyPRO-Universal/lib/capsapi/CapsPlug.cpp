4ee12e35f0fc290f158720fd9ebf061d46f1e2ba
Gianluca Renzi <icjtqr@gmail.com>
2026-01-12 00:14:24 +0100

    Update waffleCopyPRO-Universal/lib/capsapi/CapsPlug.cpp

--- waffleCopyPRO-Universal/lib/capsapi/CapsPlug.cpp
+++ waffleCopyPRO-Universal/lib/capsapi/CapsPlug.cpp
@@ -1,3 +1,5 @@
+#include "debugmsg.h"
+#include <QString>
 // Simple caps plug-in support on Win32, should fit most users
 //
 // You may want to link with the libray instead, if your product fully complies
@@ -15,6 +17,24 @@
 #include "Comtype.h"
 #include "CapsAPI.h"
 #include "CapsPlug.h"
+#ifdef CAPS_USER
+extern "C" {
+    SDWORD CAPSInit();
+    SDWORD CAPSExit();
+    SDWORD CAPSAddImage();
+    SDWORD CAPSRemImage(SDWORD id);
+    SDWORD CAPSLockImage(SDWORD id, PCHAR name);
+    SDWORD CAPSLockImageMemory(SDWORD id, PUBYTE buffer, UDWORD length, UDWORD flag);
+    SDWORD CAPSUnlockImage(SDWORD id);
+    SDWORD CAPSLoadImage(SDWORD id, UDWORD flag);
+    SDWORD CAPSGetImageInfo(PCAPSIMAGEINFO pi, SDWORD id);
+    SDWORD CAPSLockTrack(PCAPSTRACKINFO pi, SDWORD id, UDWORD cylinder, UDWORD head, UDWORD flag);
+    SDWORD CAPSUnlockTrack(SDWORD id, UDWORD cylinder, UDWORD head);
+    SDWORD CAPSUnlockAllTracks(SDWORD id);
+    PCHAR CAPSGetPlatformName(UDWORD pid);
+    SDWORD CAPSGetInfo(void* si, SDWORD id, UDWORD cylinder, UDWORD head, UDWORD inftype, UDWORD infid);
+}
+#endif
 
 
 #ifdef _WIN32
@@ -63,10 +83,32 @@ CapsProc cpr[]= {
 // start caps image support
 SDWORD CapsInit()
 {
+	DebugMsg::print(__func__, QString("ENTER"));
 	if (capi)
 		return imgeOk;
 
-#ifdef WIN32
+#ifdef CAPS_USER
+	// when linking capsimg sources directly, map function pointers to the library functions
+	cpr[0].proc = (void*)CAPSInit;
+	cpr[1].proc = (void*)CAPSExit;
+	cpr[2].proc = (void*)CAPSAddImage;
+	cpr[3].proc = (void*)CAPSRemImage;
+	cpr[4].proc = (void*)CAPSLockImage;
+	cpr[5].proc = (void*)CAPSUnlockImage;
+	cpr[6].proc = (void*)CAPSLoadImage;
+	cpr[7].proc = (void*)CAPSGetImageInfo;
+	cpr[8].proc = (void*)CAPSLockTrack;
+	cpr[9].proc = (void*)CAPSUnlockTrack;
+	cpr[10].proc = (void*)CAPSUnlockAllTracks;
+	cpr[11].proc = (void*)CAPSGetPlatformName;
+	cpr[12].proc = (void*)CAPSLockImageMemory;
+	cpr[13].proc = (void*)CAPSGetInfo;
+	// mark capi to indicate library available
+	capi = (void*)1;
+	SDWORD res = cpr[0].proc ? CAPSHOOKN(cpr[0].proc)() : imgeUnsupported;
+	return res;
+#else
+#ifdef _WIN32
 	capi = LoadLibrary(L"CAPSImg.dll");
 #else
 #ifdef __APPLE__
@@ -95,6 +137,7 @@ SDWORD CapsInit()
 	SDWORD res=cpr[0].proc ? CAPSHOOKN(cpr[0].proc)() : imgeUnsupported;
 
 	return res;
+#endif
 }
 
 // stop caps image support
@@ -102,7 +145,9 @@ SDWORD CapsExit()
 {
 	SDWORD res=cpr[1].proc ? CAPSHOOKN(cpr[1].proc)() : imgeUnsupported;
 
-	if (capi) {
+#ifndef CAPS_USER
+	if (capi)
+{
 #ifdef _WIN32
 		FreeLibrary(capi);
 #else
@@ -110,6 +155,10 @@ SDWORD CapsExit()
 #endif
 		capi=NULL;
 	}
+#else
+	// if CAPS_USER, do not attempt to free dynamic lib handle
+	capi = NULL;
+#endif
 
 	for (int prc=0; cpr[prc].name; prc++)
 		cpr[prc].proc=NULL;
@@ -120,6 +169,7 @@ SDWORD CapsExit()
 // add image container
 SDWORD CapsAddImage()
 {
+	DebugMsg::print(__func__, QString("ENTER"));
 	SDWORD res=cpr[2].proc ? CAPSHOOKN(cpr[2].proc)() : -1;
 
 	return res;
@@ -128,7 +178,12 @@ SDWORD CapsAddImage()
 // delete image container
 SDWORD CapsRemImage(SDWORD id)
 {
+	DebugMsg::print(__func__, QString("ENTER"));
+#ifdef CAPS_USER
+	SDWORD res = CAPSRemImage(id);
+#else
 	SDWORD res=cpr[3].proc ? CAPSHOOKN(cpr[3].proc)(id) : -1;
+#endif
 
 	return res;
 }
@@ -136,6 +191,7 @@ SDWORD CapsRemImage(SDWORD id)
 // lock image
 SDWORD CapsLockImage(SDWORD id, PCHAR name)
 {
+	DebugMsg::print(__func__, QString("ENTER"));
 	SDWORD res=cpr[4].proc ? CAPSHOOKN(cpr[4].proc)(id, name) : imgeUnsupported;
 
 	return res;
@@ -144,7 +200,12 @@ SDWORD CapsLockImage(SDWORD id, PCHAR name)
 // unlock image
 SDWORD CapsUnlockImage(SDWORD id)
 {
+	DebugMsg::print(__func__, QString("ENTER"));
+#ifdef CAPS_USER
+	SDWORD res = CAPSUnlockImage(id);
+#else
 	SDWORD res=cpr[5].proc ? CAPSHOOKN(cpr[5].proc)(id) : imgeUnsupported;
+#endif
 
 	return res;
 }
@@ -152,7 +213,12 @@ SDWORD CapsUnlockImage(SDWORD id)
 // load and decode complete image
 SDWORD CapsLoadImage(SDWORD id, UDWORD flag)
 {
+	DebugMsg::print(__func__, QString("ENTER"));
+#ifdef CAPS_USER
+	SDWORD res = CAPSLoadImage(id, flag);
+#else
 	SDWORD res=cpr[6].proc ? CAPSHOOKN(cpr[6].proc)(id, flag) : imgeUnsupported;
+#endif
 
 	return res;
 }
@@ -160,12 +226,19 @@ SDWORD CapsLoadImage(SDWORD id, UDWORD flag)
 // get image information
 SDWORD CapsGetImageInfo(PCAPSIMAGEINFO pi, SDWORD id)
 {
+	DebugMsg::print(__func__, QString("ENTER"));
+#ifdef CAPS_USER
+	SDWORD res = CAPSGetImageInfo(pi, id);
+#else
 	SDWORD res=cpr[7].proc ? CAPSHOOKN(cpr[7].proc)(pi, id) : imgeUnsupported;
+#endif
 
 	return res;
 }
 
-SDWORD CapsGetInfo(void* si, SDWORD id, UDWORD cylinder, UDWORD head, UDWORD inftype, UDWORD infid) {
+SDWORD CapsGetInfo(void* si, SDWORD id, UDWORD cylinder, UDWORD head, UDWORD inftype, UDWORD infid)
+{
+	DebugMsg::print(__func__, QString("ENTER"));
 	SDWORD res = cpr[13].proc ? CAPSHOOKN(cpr[13].proc)(si, id, cylinder, head, inftype, infid) : imgeUnsupported;
 
 	return res;
@@ -174,7 +247,12 @@ SDWORD CapsGetInfo(void* si, SDWORD id, UDWORD cylinder, UDWORD head, UDWORD inf
 // load and decode track, or return with the cache
 SDWORD CapsLockTrack(PCAPSTRACKINFO pi, SDWORD id, UDWORD cylinder, UDWORD head, UDWORD flag)
 {
+	DebugMsg::print(__func__, QString("ENTER"));
+#ifdef CAPS_USER
+	SDWORD res = CAPSLockTrack(pi, id, cylinder, head, flag);
+#else
 	SDWORD res=cpr[8].proc ? CAPSHOOKN(cpr[8].proc)(pi, id, cylinder, head, flag) : imgeUnsupported;
+#endif
 
 	return res;
 }
@@ -182,6 +260,7 @@ SDWORD CapsLockTrack(PCAPSTRACKINFO pi, SDWORD id, UDWORD cylinder, UDWORD head,
 // remove track from cache
 SDWORD CapsUnlockTrack(SDWORD id, UDWORD cylinder, UDWORD head)
 {
+	DebugMsg::print(__func__, QString("ENTER"));
 	SDWORD res=cpr[9].proc ? CAPSHOOKN(cpr[9].proc)(id, cylinder, head) : imgeUnsupported;
 
 	return res;
@@ -190,6 +269,7 @@ SDWORD CapsUnlockTrack(SDWORD id, UDWORD cylinder, UDWORD head)
 // remove all tracks from cache
 SDWORD CapsUnlockAllTracks(SDWORD id)
 {
+	DebugMsg::print(__func__, QString("ENTER"));
 	SDWORD res=cpr[10].proc ? CAPSHOOKN(cpr[10].proc)(id) : imgeUnsupported;
 
 	return res;
@@ -198,6 +278,7 @@ SDWORD CapsUnlockAllTracks(SDWORD id)
 // get platform name
 PCHAR CapsGetPlatformName(UDWORD pid)
 {
+	DebugMsg::print(__func__, QString("ENTER"));
 	PCHAR res=cpr[11].proc ? CAPSHOOKS(cpr[11].proc)(pid) : NULL;
 
 	return res;
@@ -206,6 +287,7 @@ PCHAR CapsGetPlatformName(UDWORD pid)
 // lock memory mapped image
 SDWORD CapsLockImageMemory(SDWORD id, PUBYTE buffer, UDWORD length, UDWORD flag)
 {
+	DebugMsg::print(__func__, QString("ENTER"));
 	SDWORD res=cpr[12].proc ? CAPSHOOKN(cpr[12].proc)(id, buffer, length, flag) : imgeUnsupported;
 
 	return res;
