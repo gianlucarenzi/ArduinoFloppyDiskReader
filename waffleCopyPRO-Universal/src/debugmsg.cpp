5b824d65905fe94f521bd9d86282986f0d3981e8
Gianluca Renzi <icjtqr@gmail.com>
2026-01-12 00:14:25 +0100

    Update waffleCopyPRO-Universal/src/debugmsg.cpp

--- waffleCopyPRO-Universal/src/debugmsg.cpp
+++ waffleCopyPRO-Universal/src/debugmsg.cpp
@@ -1,5 +1,6 @@
 #include "debugmsg.h"
 #include <QTextStream>
+#include <QString>
 #include <stdio.h>
 
 bool DebugMsg::s_enabled = false;
@@ -7,6 +8,11 @@ bool DebugMsg::s_enabled = false;
 void DebugMsg::init(bool enabled)
 {
     s_enabled = enabled;
+}
+
+bool DebugMsg::enabled()
+{
+    return s_enabled;
 }
 
 void DebugMsg::print(const char* func, const QString& msg)
