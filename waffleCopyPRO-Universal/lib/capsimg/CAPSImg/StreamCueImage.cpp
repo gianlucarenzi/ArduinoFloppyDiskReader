d68ed964a97297ee1c329d18a27d2a3c7306709f
Gianluca Renzi <icjtqr@gmail.com>
2026-01-12 00:14:26 +0100

    Update waffleCopyPRO-Universal/lib/capsimg/

---
+++ waffleCopyPRO-Universal/lib/capsimg/CAPSImg/StreamCueImage.cpp
@@ -0,0 +1,24 @@
+#include "debugmsg.h"
+#include "stdafx.h"
+
+
+
+CStreamCueImage::CStreamCueImage()
+{
+}
+
+CStreamCueImage::~CStreamCueImage()
+{
+}
+
+// lock and scan file
+int CStreamCueImage::Lock(PCAPSFILE pcf)
+{
+	return imgeOk;
+}
+
+// release file
+int CStreamCueImage::Unlock()
+{
+	return imgeOk;
+}
---
+++ waffleCopyPRO-Universal/lib/capsimg/Core/BaseFile.cpp
@@ -0,0 +1,20 @@
+#include "debugmsg.h"
+#include "stdafx.h"
+
+
+
+CBaseFile::CBaseFile()
+{
+	Clear();
+}
+
+CBaseFile::~CBaseFile()
+{
+}
+
+// clear file settings
+void CBaseFile::Clear()
+{
+	fileopen=0;
+	filemode=0;
+}
