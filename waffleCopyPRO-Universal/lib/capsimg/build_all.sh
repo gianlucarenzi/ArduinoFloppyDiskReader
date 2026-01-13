dbe5206a2c8ab54e8561fc63d0fd7316900f56dd
Gianluca Renzi <icjtqr@gmail.com>
2026-01-13 23:44:38 +0100

    Must be used selecting the correct architecture: x86 or i686

--- waffleCopyPRO-Universal/lib/capsimg/build_all.sh
+++ waffleCopyPRO-Universal/lib/capsimg/build_all.sh
@@ -11,9 +11,20 @@ make CROSS_COMPILE= all
 make CROSS_COMPILE= build
 make CROSS_COMPILE= clean
 
-echo "----- Windows cross (mingw): build"
+echo "----- Windows cross (mingw x86-64): build"
 make CROSS_COMPILE=x86_64-w64-mingw32- all
 make CROSS_COMPILE=x86_64-w64-mingw32- build
 make CROSS_COMPILE=x86_64-w64-mingw32- clean
+
+# Rename CAPSImg.dll into CAPSImg-x86.dll
+mv CAPSImg.dll CAPSImg-x86.dll
+
+echo "----- Windows cross (mingw i686-64): build"
+make CROSS_COMPILE=i686-w64-mingw32- all
+make CROSS_COMPILE=i686-w64-mingw32- build
+make CROSS_COMPILE=i686-w64-mingw32- clean
+
+# Rename CAPSImg.dll into CAPSImg-i686.dll
+mv CAPSImg.dll CAPSImg-i686.dll
 
 echo "All builds completed."
